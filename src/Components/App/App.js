6449e85b1381bd10866b3a29e84f0b074e51e575
Zachary Campanelli <84996750+Zacharycampanelli@users.noreply.github.com>
2023-02-03 16:54:31 -0500

    method added to save to a user's spotify account

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -25,6 +25,7 @@ class App extends React.Component {
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
     this.updatePlaylistName = this.updatePlaylistName.bind(this);
+    this.savePlaylist = this.savePlaylist.bind(this);
   
   }
 
@@ -50,6 +51,10 @@ class App extends React.Component {
     this.setState({playlistName: name})
   }
 
+  savePlaylist() {
+    const trackUris = this.state.playlistTracks.map(track => track.uri);
+  }
+
   render() {
     return (
       <div>
@@ -62,7 +67,8 @@ class App extends React.Component {
             <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack} />
             <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} 
             onRemove={this.removeTrack}
-            onNameChange={this.updatePlaylistName} />
+            onNameChange={this.updatePlaylistName} 
+            onSave={this.savePlaylist} />
           </div>
         </div>
       </div>
