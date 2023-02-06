a05e759fd4f3dd58e441786505affee8c9697a10
Zachary Campanelli <84996750+Zacharycampanelli@users.noreply.github.com>
2023-02-06 16:43:07 -0500

    Search request sent through Spotify Web API

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -21,9 +21,31 @@ const Spotify = {
       window.history.pushState('Access Token', null, '/');
       return accessToken;
     } else {
-        const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUri}`
-        window.location = accessUrl;
+      const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUri}`;
+      window.location = accessUrl;
     }
+  },
+
+  search(term) {
+    // const endpoint = 'https://api.spotify.com/v1/search?type=track&q=TERM';
+    const accessToken = Spotify.getAccessToken;
+    return fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`, {
+      headers: { Authorization: `Bearer ${accessToken}` },
+    }).then(response => {
+        return response.json();
+    }).then(jsonRes => {
+        if(!jsonRes.tracks) {
+            return [];
+        }
+        return jsonRes.tracks.items.map(track => 
+            ({
+                id: track.id,
+                name: track.name,
+                artist: track.artists[0].name,
+                album: track.album.name,
+                uri: track.uri
+            }))
+    })
   },
 };
 export default Spotify;
